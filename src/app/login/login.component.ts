f1c908e6e87d089ef61399b2a6f8ccb78b77c313
李英宗 <neil.lee@udngroup.com.tw>
2022-07-31 13:38:35 +0800

    任務 04：在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
+import { UserLoginInfo } from './../interfaces/login-info';
+import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = {
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    this.loginService
+      .login(this.user)
+      .subscribe({
+        next: userInfo => {
+          localStorage.setItem('token', userInfo.user.token);
+          this.router.navigate(['/']);
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.error.body[0]);
+        }
+      });
+
   }
 
 }
