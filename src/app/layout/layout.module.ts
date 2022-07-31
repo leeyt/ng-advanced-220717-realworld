26a1b2087263847ce38a1fdc537df94dc0e4ac93
李英宗 <neil.lee@udngroup.com.tw>
2022-08-01 00:16:03 +0800

    練習：將 header 和 footer 拆成個別的元件

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
